9c681be5fc9f1a0b203b1272611af6ed8fa4f31b
Vjekoslav Nikolic <vjeko.nikolic@extern.evocenta.com>
2023-03-06 14:08:01 +0100

    Pull out gtl, prepare all.py

--- gtl_poc/all.py
+++ gtl_poc/all.py
@@ -22,13 +22,15 @@ binary = RandomBinaryData(inputs, categories, num_cousins, examples, cousin_size
 if not os.path.exists(f'{checkpoint_dir_name}/tl_base.ckpt'):
     multilayer = train_multilayer(data=binary.data(), num_categories=categories, name='multilayer_10_categ_tl_base')
     multilayer.save_checkpoint(f'{checkpoint_dir_name}/tl_base.ckpt')
+else:
+    multilayer = MaskedModel.load_from_checkpoint(f"{checkpoint_dir_name}/tl_base.ckpt")
 
 all_cousins = []
 for i in range(num_cousins):
     print(f"cousins {i}")
     cousin = train_multilayer(data=binary.data(i), name='cousins', checkpoint='tl_base', convergence=False)
     all_cousins.append(cousin.model.state_dict())
-mask = create_mask(cousins=all_cousins, model=MaskedModel.load_from_checkpoint(f'{checkpoint_dir_name}/tl_base.ckpt'))
+mask = create_mask(cousins=all_cousins, model=multilayer)
 torch.save(mask, f'{checkpoint_dir_name}/mask.pt')
 
 for i in range(num_cousins):
@@ -38,3 +40,47 @@ for i in range(num_cousins):
     print(f"situation_tl_masked {i}")
     situation_masked = train_multilayer(data=data, name='situation_tl_masked', checkpoint='tl_base', mask=mask,
                                         convergence=False)
+# rewrite - how to add a function after loss.backward() and before optimizers are stepped in pytorch?
+# In PyTorch, you can add a function after loss.backward() and before the optimizer's step() method by defining a closure that wraps the backward pass and the optimization step. You can then add your additional function between the backward pass and the optimization step in the closure.
+#
+# Here's an example of how to add a function after loss.backward() and before the optimizer's step() method:
+#
+# python
+#
+# import torch
+# import torch.optim as optim
+#
+# # Define the model, loss function, and optimizer
+# model = MyModel()
+# loss_fn = torch.nn.CrossEntropyLoss()
+# optimizer = optim.SGD(model.parameters(), lr=0.1)
+#
+# # Define a closure that wraps the backward pass and the optimization step
+# def closure():
+#     optimizer.zero_grad()
+#     outputs = model(inputs)
+#     loss = loss_fn(outputs, targets)
+#     loss.backward()
+#     # Call your additional function here
+#     additional_function()
+#     return loss
+#
+# # Perform a training step
+# inputs = torch.randn(10, 3)
+# targets = torch.LongTensor([0, 1, 2, 1, 0, 2, 2, 1, 0, 1])
+# loss = optimizer.step(closure)
+#
+# # Define your additional function
+# def additional_function():
+#     with torch.no_grad():
+#         # Perform some additional operation on the model's parameters
+#         for param in model.parameters():
+#             param.add_(0.1 * param.grad)
+#
+# In this example, we define a model, loss function, and optimizer. We define a closure closure that wraps the backward pass and the optimization step. The closure calls optimizer.zero_grad() to clear the gradients, computes the forward pass of the model on inputs, calculates the loss with loss_fn, and calls loss.backward() to compute the gradients of the loss with respect to the model's parameters.
+#
+# We then call our additional function additional_function() between the backward pass and the optimization step in the closure. Finally, we return the loss from the closure.
+#
+# We perform a training step by calling optimizer.step(closure) with the closure as its argument. The optimizer will call the closure to perform the backward pass and the optimization step.
+#
+# We define our additional function additional_function() outside of the closure. In this case, the function performs some additional operation on the model's parameters by adding 0.1 * param.grad to each parameter.
--- gtl_poc/libs/gtl.py
+++ gtl_poc/libs/gtl.py
@@ -1,4 +1,5 @@
 import torch
+from torch.utils.data import DataLoader
 
 
 def apply_mask(mask, model):
@@ -9,12 +10,11 @@ def apply_mask(mask, model):
 
 
 def create_mask(cousins, model):
-    def learning_spread(model_layer, cousin_layer):
-        ls = torch.mean((cousin_layer - model_layer) ** 2, 0)
-        min_ls = torch.min(ls)
-        max_ls = torch.max(ls)
-        spread = max_ls - min_ls
-        return (ls - min_ls) / spread
+    def get_layer_mask(model_layer, cousin_layer):
+        learning_spread = torch.mean((cousin_layer - model_layer) ** 2, 0)
+        min_ls = torch.min(learning_spread)
+        max_ls = torch.max(learning_spread)
+        return (learning_spread - min_ls) / (max_ls - min_ls)
 
     mask = {}
     model_dict = model.state_dict()
@@ -26,6 +26,27 @@ def create_mask(cousins, model):
             cousins_stack[name].append(cousin_dict[name])  # TODO: this should be done differently...
 
     for name, param in model.named_parameters():
-        mask[name] = learning_spread(model_dict[name], torch.stack(cousins_stack[name]))
+        mask[name] = get_layer_mask(model_dict[name], torch.stack(cousins_stack[name]))
 
     return mask
+
+
+def _create_cousin(model, data):
+    cousin = model.clone()
+    cousin.train()
+    cousin_data = DataLoader(data, batch_size=32, shuffle=True, num_workers=4)
+    for batch in cousin_data:
+        x, y = batch
+        loss = cousin(x, y)
+        cousin.backward(loss)
+        cousin.step()
+        cousin.zero_grad()
+    return cousin
+
+
+def create_cousins(model, data):
+    cousins = []
+    for cousin_data in data:
+        cousin = _create_cousin(model, cousin_data)
+        cousins.append(cousin.model.state_dict())
+    return cousins
